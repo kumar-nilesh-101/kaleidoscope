d189a1f018e25247f5cc351ffa93b775494b565b
Nilesh-Kumar-DS <apple@Nilesh-Kumar-DS.local>
2024-08-16 05:28:59 +0530

    jest setup completed

---
+++ __tests__/design/creational/factory/factory.decorator.spec.ts
@@ -0,0 +1,7 @@
+import {Factory} from '@design/creational/factory';
+
+describe('Factory design pattern', () => {
+  describe('Factory decorator', () => {
+    it('should add metadata to factory class', () => {});
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,29 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   rootDir: './',
   coverageDirectory: '<rootDir>/coverage',
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/**/constant.ts',
+    '!<rootDir>/src/**/*.types.ts',
+    '!<rootDir>/src/**/*.interface.ts',
+    '!<rootDir>/src/**/*.base.ts',
   ],
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>'],
   testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist'],
   coverageReporters: ['json', 'html'],
   transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+    'tests/(.*)': '<rootDir>/__tests__/$1',
+  },
+  testRegex: '.spec.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  verbose: true,
 };
